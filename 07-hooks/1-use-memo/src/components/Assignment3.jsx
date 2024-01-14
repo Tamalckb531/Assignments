e2bd32c936aea391ff8f1789c6c43f034ccc310c
Tamal Chakraborty <ckbtamaldipnew@gmail.com>
2024-01-14 10:50:34 +0600

    week 6 assignment completed

--- 07-hooks/1-use-memo/src/components/Assignment3.jsx
+++ 07-hooks/1-use-memo/src/components/Assignment3.jsx
@@ -6,13 +6,18 @@ const Assignment3 = () => {
     const [items, setItems] = useState([
         { name: 'Chocolates', value: 10 },
         { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
+        { name: 'Onion', value: 40 },
+        { name: 'Tomato', value: 40 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(()=>{
+        return items.reduce((accumulator, item)=>{
+            return accumulator + item.value;
+        }, 0);
+    },[items]);
+
     // Your code ends here
     return (
         <div>
--- 07-hooks/2-use-callback/src/components/Assignment1.jsx
+++ 07-hooks/2-use-callback/src/components/Assignment1.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { memo, useCallback, useState } from "react";
 
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
@@ -6,13 +6,19 @@ export function Assignment1() {
     const [count, setCount] = useState(0);
 
     // Your code starts here
-    function handleIncrement() {
+    const handleIncrement = useCallback(() => {
+        setCount(function(currentCount){
+            return currentCount+1;
+        });
+    }, [])
 
-    }
+    const handleDecrement = useCallback(() => {
+        setCount(function(currentCount){
+            return currentCount-1;
+        });
+    }, [])
 
-    function handleDecrement() {
-        
-    }
+    //* The way we wrote the setCount is because it don't depend on count and hence we don't need to give any dependencies to useCallback hooks. If we give the count as an dependency and use the traditional setCount method then there will be no significance of making the function with useCallback. count is the only state here that is responsible for rerendering and we don't want the buttons to redender each time.
     // Your code ends here
 
     return (
@@ -23,9 +29,9 @@ export function Assignment1() {
     );
 };
 
-const CounterButtons = ({ onIncrement, onDecrement }) => (
+const CounterButtons = memo(({ onIncrement, onDecrement }) => (
     <div>
         <button onClick={onIncrement}>Increment</button>
         <button onClick={onDecrement}>Decrement</button>
     </div>
-);
+));
