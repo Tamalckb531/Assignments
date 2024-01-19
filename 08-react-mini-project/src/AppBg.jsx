bda251b15d6dcf02d1983b0a695876d2e44ff8b2
Tamal Chakraborty <ckbtamaldipnew@gmail.com>
2024-01-19 22:11:13 +0600

    project 2 added

--- 08-react-mini-project/src/AppBg.jsx
+++ 08-react-mini-project/src/AppBg.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './AppBg.css'
 
 const AppBg = () => {
+  const [bgColor, setBgColor] = useState('white');
+
+  useEffect(()=>{
+    document.body.style.backgroundColor = bgColor;
+  }, [bgColor]);
+
+  function handleClick(ele)
+  {
+    const newColor = ele.style.backgroundColor;
+    if(newColor === 'orange') {
+      setBgColor('white');
+      return;
+    }
+    setBgColor(newColor);
+  }
+
   return (
-    <div>AppBg</div>
+    <>
+      <div className="bar">
+        <button style={{background:"red"}} className='custom-btn' onClick={(e)=>handleClick(e.target)}>Red</button>
+        <button style={{background:"yellow"}} className='custom-btn' onClick={(e)=>handleClick(e.target)}>Yellow</button>
+        <button style={{background:"black", color: 'white'}} className='custom-btn' onClick={(e)=>handleClick(e.target)}>Black</button>
+        <button style={{background:"purple"}} className='custom-btn' onClick={(e)=>handleClick(e.target)}>Purple</button>
+        <button style={{background:"green"}} className='custom-btn' onClick={(e)=>handleClick(e.target)}>Green</button>
+        <button style={{background:"blue"}} className='custom-btn' onClick={(e)=>handleClick(e.target)}>Blue</button>
+        <button style={{background:"orange"}} className='custom-btn'onClick={(e)=>handleClick(e.target)}>Default</button>
+      </div>
+    </>
   )
 }
+
 
 export default AppBg
--- 08-react-mini-project/src/main.jsx
+++ 08-react-mini-project/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './AppProfile.jsx'
-import './index.css'
 import AppBg from './AppBg.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
