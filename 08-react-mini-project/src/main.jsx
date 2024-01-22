981dd8416c0dc24d9e7ef61e145c48f8959a900d
Tamal Chakraborty <ckbtamaldipnew@gmail.com>
2024-01-22 19:43:39 +0600

    assignment 4 added

--- 08-react-mini-project/src/main.jsx
+++ 08-react-mini-project/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './AppProfile.jsx'
-import AppBg from './AppBg.jsx'
+// import AppBg from './AppBg.jsx'
+import AppPara from './AppPara.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <App />
-    <AppBg/>
+    // <AppBg/>
+    <AppPara/>
 )
